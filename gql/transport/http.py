7ddb3548d24342a0d3c18d0b7b1c79a2d5c381c3
Mariusz Gaweł <mariusz.gawel@esky.com>
2017-07-03 12:05:39 +0200

    Add argument to verify SSL certification or no

--- gql/transport/http.py
+++ gql/transport/http.py
@@ -1,6 +1,7 @@
 class HTTPTransport(object):
 
-    def __init__(self, url, headers=None, cookies=None):
+    def __init__(self, url, headers=None, cookies=None, verify=None):
         self.url = url
         self.headers = headers
         self.cookies = cookies
+		self.verify = verify
